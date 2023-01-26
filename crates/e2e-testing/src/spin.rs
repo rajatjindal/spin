ee8f3e62855eb21606e51a8bc66a956bafdf88b2
Rajat Jindal <rajatjindal83@gmail.com>
2023-01-26 22:35:20 +0530

    fix ci failures
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- crates/e2e-testing/src/spin.rs
+++ crates/e2e-testing/src/spin.rs
@@ -1,17 +1,29 @@
 use crate::utils;
 use anyhow::Result;
-use lockfile::Lockfile;
 use std::path::{Path, PathBuf};
 use std::process::Output;
-use std::time::Duration;
-use waitfor::wait_for;
+use std::sync::Mutex;
 
-const INSTALLING_PLUGINS_LOCK: &str = "/tmp/installing-plugins.lock";
+#[cfg(target_family = "unix")]
+use {
+    nix::sys::signal::{kill, Signal},
+    nix::unistd::Pid,
+};
+
+static INSTALLING_TEMPLATES_MUTEX: Mutex<i32> = Mutex::new(0);
+static INSTALLING_PLUGINS_MUTEX: Mutex<i32> = Mutex::new(0);
 
 pub fn template_install(mut args: Vec<&str>) -> Result<Output> {
     let mut cmd = vec!["spin", "templates", "install"];
     cmd.append(&mut args);
-    utils::run(cmd, None, None)
+
+    let mut x = INSTALLING_TEMPLATES_MUTEX.lock().unwrap();
+    let result = utils::run(cmd, None, None);
+
+    //this ensure we have mutex lock until here
+    *x += 1;
+
+    result
 }
 
 pub fn new_app(template_name: &str, app_name: &str) -> Result<Output> {
@@ -27,18 +39,9 @@ pub fn new_app(template_name: &str, app_name: &str) -> Result<Output> {
 }
 
 pub fn install_plugins(plugins: Vec<&str>) -> Result<Output> {
-    // prevent running multiple `install plugins` at same time,
-    // https://github.com/fermyon/spin/issues/997
-    wait_for::<_, _, ()>(Duration::from_secs(30), Duration::from_secs(1), || {
-        if Path::new(INSTALLING_PLUGINS_LOCK).exists() {
-            Ok(None)
-        } else {
-            Ok(Some("install plugins not running"))
-        }
-    })
-    .unwrap();
+    // lock mutex to ensure one install_plugins runs at a time
+    let mut x = INSTALLING_PLUGINS_MUTEX.lock().unwrap();
 
-    let lockfile = Lockfile::create(INSTALLING_PLUGINS_LOCK).unwrap();
     let mut output = utils::run(vec!["spin", "plugin", "update"], None, None)?;
 
     for plugin in plugins {
@@ -49,7 +52,9 @@ pub fn install_plugins(plugins: Vec<&str>) -> Result<Output> {
         )?;
     }
 
-    lockfile.release()?;
+    //this ensure we have mutex lock until here
+    *x += 1;
+
     Ok(output)
 }
 
@@ -61,4 +66,23 @@ pub fn build_app(appname: &str) -> Result<Output> {
 pub fn appdir(appname: &str) -> String {
     let dir = Path::new(utils::testcases_base_dir().as_str()).join(appname);
     dir.into_os_string().into_string().unwrap()
+}
+
+#[cfg(target_family = "unix")]
+pub async fn stop_app(process: &mut tokio::process::Child) -> Result<(), anyhow::Error> {
+    let pid = process.id().unwrap();
+    println!("stopping app with pid {}", pid);
+    let pid = Pid::from_raw(pid as i32);
+    kill(pid, Signal::SIGINT).map_err(anyhow::Error::msg)
+}
+
+#[cfg(target_family = "windows")]
+pub async fn stop_app(process: &mut tokio::process::Child) -> Result<(), anyhow::Error> {
+    // stop the app at the end of testcase
+    let _ = &mut process.kill().await.map_err(anyhow::Error::msg);
+
+    match process.wait().await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(anyhow::Error::msg(e)),
+    }
 }
--- crates/e2e-testing/src/testcase.rs
+++ crates/e2e-testing/src/testcase.rs
@@ -1,4 +1,5 @@
-use crate::controller::{AppInstance, Controller};
+use crate::controller::Controller;
+use crate::metadata_extractor::AppMetadata;
 use crate::spin;
 use crate::utils;
 use anyhow::{Context, Result};
@@ -53,7 +54,7 @@ pub struct TestCase {
     pub pre_build_hooks: Option<Vec<Vec<String>>>,
 
     /// assertions to run once the app is running
-    pub assertions: fn(app: &AppInstance) -> Result<()>,
+    pub assertions: fn(app: &AppMetadata) -> Result<()>,
 }
 
 impl TestCase {
@@ -117,11 +118,17 @@ impl TestCase {
             .await
             .context("deploying app")?;
 
+        let metadata = app.metadata;
+
         // run test specific assertions
         let assert_fn = self.assertions;
 
-        return task::spawn_blocking(move || assert_fn(&app))
+        task::spawn_blocking(move || assert_fn(&metadata))
             .await
-            .context("running testcase specific assertions")?;
+            .context("running testcase specific assertions")
+            .unwrap()?;
+
+        let mut process = app.process.unwrap();
+        spin::stop_app(&mut process).await
     }
 }
